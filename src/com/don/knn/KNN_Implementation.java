9af7d57a2744a302ca8ae30aa64c88896029ec5a
BelHaj Nasr Iskander <skander.ph@live.fr>
2020-11-07 23:46:19 +0100

    add implementation knn eucludienne

--- src/com/don/knn/KNN_Implementation.java
+++ src/com/don/knn/KNN_Implementation.java
@@ -1,7 +1,10 @@
 package com.don.knn;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 /*
@@ -27,12 +30,52 @@ public class KNN_Implementation {
 		trainfeatures.add(new double[0]); // add values to be calculated like conf of benificier
 		trainlabel.add("benificier"); // benificier atttribuated
 	}
+
 	/*
 	 * loading testing data and extracting features and label for training dataset
 	 * 
 	 */
 	void loadtestData() {
-		testfeatures.add(new double[0]); //info of donation to be classed
+		testfeatures.add(new double[0]); // info of donation to be classed
 
+	}
+
+	/*
+	 * Based on user input, calling algorithm to calculate distance
+	 */
+	void distanceCalcualte() throws IOException {
+		euclideanDistance();
+	}
+
+	/*
+	 * EuclideanDistance Calling euclidean method to calculate distance and writing
+	 * output to file.
+	 * 
+	 */
+
+	@SuppressWarnings("unchecked")
+	void euclideanDistance() throws FileNotFoundException {
+		KNN_Distance euclidean = new KNN_Distance();
+		Iterator<double[]> testITR = testfeatures.iterator();
+		while (testITR.hasNext()) {
+			double testF[] = testITR.next();
+			Iterator<double[]> trainITR = trainfeatures.iterator();
+			int noOfobject = 0;
+			ArrayList<DistanceAndFeatures> ts = new ArrayList<>();
+			while (trainITR.hasNext()) {
+				double trainF[] = trainITR.next();
+				double dist = 0;
+				dist = euclidean.getEuclideanDistance(trainF, testF);
+
+				String trainFeat = trainlabel.get(noOfobject);
+				DistanceAndFeatures DfObject = new DistanceAndFeatures(dist, trainFeat);
+				ts.add(DfObject);
+				Collections.sort(ts);
+				noOfobject++;
+			}
+			
+			//TODO Iskander class finding
+
+		}
 	}
 }
